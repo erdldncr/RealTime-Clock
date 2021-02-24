cc12074a8639660d4f7b331046989a83725c8c00
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-02-24 00:33:13 +0000

    js rules have been set

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 
+function showDate(){
+    let date = new Date()
+    let hours=date.getHours()
+    let minutes=date.getMinutes()
+    let seconds=date.getSeconds()
+    document.querySelector('.hours').innerHTML=hours
+    document.querySelector('.minutes').innerHTML=minutes
+    document.querySelector('.seconds').innerHTML=seconds
+    updateTime()
+}
+function updateTime(){
+ setTimeout('showDate()',1000)
+}
 
